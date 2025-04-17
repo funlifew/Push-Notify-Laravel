523fcfaf7bb6c177182813d4d6661ad3192a537b
Mehdi Radfar <81019265+funlifew@users.noreply.github.com>
2025-04-18 02:13:25 +0330

    🗑️ Remove some files i didn't need them anymore

--- resources/views/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="row">
-    <div class="col-md-12">
-        <div class="copyright">
-            <p>Made with <3 in <a href="https://gilasweb.ir">Gilasweb</a>.</p>
-        </div>
-    </div>
-</div>
