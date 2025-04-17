4305b268bf62ba2a1027a016f88e78e62ea6c8c1
Mehdi Radfar <81019265+funlifew@users.noreply.github.com>
2025-04-17 14:46:53 +0330

    🔥 Add ServiceProvider

---
+++ src/PushNotifyServiceProvider.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Funlifew\PushNotify;
+
+use Funlifew\PushNotify\Console\Commands\GenerateToken;
+use Funlifew\PushNotify\Console\Commands\InstallPush;
+use Funlifew\PushNotify\Console\Commands\SendScheduledNotifications;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class PushNotifyServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        // Register commands
+        $this->commands([
+            GenerateToken::class,
+            InstallPush::class,
+            SendScheduledNotifications::class,
+        ]);
+
+        // Merge config
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/push-notify.php', 'push-notify'
+        );
+
+        // Register helpers
+        if (file_exists($helperFile = __DIR__ . '/helpers.php')) {
+            require_once $helperFile;
+        }
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        // Publish config
+        $this->publishes([
+            __DIR__ . '/../config/push-notify.php' => config_path('push-notify.php'),
+        ], 'push-notify-config');
+
+        // Publish assets
+        $this->publishes([
+            __DIR__ . '/../resources/js/sw.js' => public_path('sw.js'),
+            __DIR__ . '/../resources/js/subscription.js' => public_path('vendor/push-notify/js/subscription.js'),
+            __DIR__ . '/../resources/js/offline.html' => public_path('offline.html'),
+            __DIR__ . '/../resources/images/default-icon.png' => public_path('default-icon.png'),
+            __DIR__ . '/../resources/images/badge-icon.png' => public_path('badge-icon.png'),
+        ], 'push-notify-assets');
+
+        // Publish views
+        $this->publishes([
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/push-notify'),
+        ], 'push-notify-views');
+
+        // Load routes
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        
+        // Load views
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'push-notify');
+        
+        // Load migrations
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        
+        // Configure Bootstrap pagination
+        Paginator::useBootstrap();
+        
+        // Register scheduled tasks
+        $this->app->booted(function () {
+            $schedule = $this->app->make(Schedule::class);
+            $schedule->command('push:send-scheduled')->everyMinute();
+        });
+        
+        // Register middleware
+        Route::aliasMiddleware('push-notify.cors', Http\Middlewares\AllowCorsMiddleware::class);
+        Route::aliasMiddleware('push-notify.csrf', Http\Middlewares\DisableCsrfMiddleware::class);
+    }
+}
